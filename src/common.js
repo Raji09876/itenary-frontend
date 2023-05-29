131d386179fb187b4d1a588d4c71c6280269582c
sivadasu34 <112117341+sivadasu34@users.noreply.github.com>
2023-05-30 04:31:05 +0530

    changes for Home page to make api call, removed unused files

---
+++ src/common.js
@@ -0,0 +1,10 @@
+export const getImageUrl = (path) => {
+    return "/images/"+path
+}
+
+export const getItenararyUrl = (id) => {
+    return "/itenarary/"+id
+} 
+export const getPlaceUrl = (id) => {
+    return "/place/"+id
+} 
